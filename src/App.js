270b20319f7e62d6f9bb54bcb5b24553611de2ed
Scotterpop73 <sctzone@aol.com>
2022-12-28 09:45:46 -0800

    Testing creating a table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+  
+// Example of a data array that
+// you might receive from an API
+const data = [
+  { name: "Scott", age: 24, gender: "Male" },
+  { name: "Sophie", age: 36, gender: "Female" },
+  { name: "Danny", age: 24, gender: "Male"},
+]
+  
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Gender</th>
+          <th>12/28</th>
+        </tr>
+        {data.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.name}</td>
+              <td>{val.age}</td>
+              <td>{val.gender}</td>
+              <td><Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group></td>
+            </tr>
+          )
+        })}
+      </table>
     </div>
   );
 }
-
+  
 export default App;
